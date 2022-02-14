d5ed7fa8622a865dc0ef4a7a13e9abb790916eb6
itspacchu <37984032+itspacchu@users.noreply.github.com>
2022-02-14 22:38:03 +0530

    newton fractal with set roots a,b,c

--- newton.cpp
+++ newton.cpp
@@ -4,11 +4,14 @@
 int main(){
     InitWindow(800, 600, "[GPU] Newton Solver rewrite");
     Shader NewtonShader = LoadShader(0, "shaders/newton.fs");
+    int iTime_int = GetShaderLocation(NewtonShader, "iTime");
 
     while(!WindowShouldClose()){
         BeginDrawing();
             ClearBackground(RAYWHITE);
             BeginShaderMode(NewtonShader);
+                float iTime = GetTime();
+                SetShaderValue(NewtonShader, iTime_int, &iTime , SHADER_UNIFORM_FLOAT);  
                 DrawRectangle(0, 0, 800, 600, WHITE);
             EndShaderMode();
         EndDrawing();
