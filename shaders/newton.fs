593f4194637fdee074cea47ae7ca476c60aee21f
itspacchu <37984032+itspacchu@users.noreply.github.com>
2022-02-15 12:22:51 +0530

    vectorize shaders

--- shaders/newton.fs
+++ shaders/newton.fs
@@ -6,13 +6,15 @@ uniform vec2 iResolution;
 uniform float iTime;
 uniform vec3 roots;
 
+uniform vec2 coordMove = vec2(0.12,-0.35);
+
 in vec4 fragColor;
 
 
 out vec4 outColor;
 
 float m_sin(float t){
-    return sin(t/10)*0.5 + 0.5;
+    return sin(2*t)*0.5 + 0.5;
 }
 
 
@@ -26,28 +28,28 @@ vec2 cdiv(vec2 a, vec2 b) {
 
 vec2 fn(vec2 z) { // f(z) = z^ - 1
     // return cmul(z,cmul(z,z)) - vec2(iTime,0);
-    float a = roots.x;
-    float b = roots.y;
-    float c = roots.z; 
-    return cmul(z,cmul(z,z)) - cmul(z,z)*(a+b+c) - z*(c*(a+b)) - c*a*b;
+    vec2 a = vec2(3,3);
+    vec2 b = vec2(2.1+0.1*m_sin(iTime/3),-0.62+m_sin(iTime/5.2 + PI/12));
+    vec2 c = vec2(2.1+0.1*m_sin(iTime/3),0.62+m_sin(iTime/5.2 + PI/12)); 
+    return cmul(z,cmul(z,z)) - cmul(cmul(z,z),(a+b+c)) - cmul(z,cmul(c,(a+b))) - cmul(a,cmul(b,c));
 
 }
 vec2 dfn(vec2 z) { // f'(z) = 3*z^2
     // return cmul(vec2(3,0),cmul(z,z)) + vec2(0,iTime);
-    float a = roots.x;
-    float b = roots.y;
-    float c = roots.z; 
-    return 3*cmul(z,z) - 2*z*(a+b+c) - c*(a+b);
+    vec2 a = vec2(3,3);
+    vec2 b = vec2(2.1+0.1*m_sin(iTime/10),-0.62+m_sin(iTime/15.2 + PI/12));
+    vec2 c = vec2(2.1+0.1*m_sin(iTime/10),0.62+m_sin(iTime/15.2 + PI/12)); 
+    return 3*cmul(z,z) - 2*cmul(z,(a+b+c)) - cmul(c,(a+b));
 }
 
 
 void main(){
     vec2 uv = 2.0*(gl_FragCoord.xy - iResolution.xy/2)/min(iResolution.x, iResolution.y) ;
-    vec2 z = uv;
-    vec2 zp = uv;
+    highp vec2 z = (uv + coordMove)/(5.0*m_sin(iTime/30 + PI/2));
+    highp vec2 zp = z;
     float i = 0.0;
-    float thresh = 0.0001;
-    for(i = 0; i < 100.0; i++) {
+    highp float thresh = 0.00001;
+    for(i = 0; i < 50.0; i++) {
         z -= cdiv(fn(z), dfn(z)); // newton fractal
         if(length(z - zp) < thresh) break;
         zp = z;
@@ -57,15 +59,15 @@ void main(){
     float rot = mod(theta/(2*PI),1);
 
     vec3 color;
-    if(rot < 0.33){
-        color = vec3(0.8941*m_sin(iTime), 0.4745*m_sin(iTime + PI/1.21), 1.0);
+    if(rot < 0.3){
+        color = vec3(0.5255, 0.2588, 0.2588);
     }
-    else if(rot < 0.66){
-        color = vec3(0.6196*m_sin(iTime+0.123), 0.4431*m_sin(iTime + 0.453 + PI/1.21), 1.0);
+    else if(rot < 0.6){
+        color = vec3(0.5725, 0.5451, 0.8314);
     }
     else{
-        color = vec3(0.6353, 1.0, 0.8314);
+        color = vec3(0.4784, 0.7059, 0.5373);
     }
-    outColor = vec4(color*(log(i/5)*0.25+0.1),1.0);
 
+    outColor = vec4(color*(log(i/4)*0.5+0.1),1.0);
 }
